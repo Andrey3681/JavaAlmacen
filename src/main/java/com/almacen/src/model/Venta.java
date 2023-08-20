0d94485ca0f8a830c9367bb0aff1e174c577b993
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2023-08-20 00:14:27 -0500

    agrege un atributo a venta

--- src/main/java/com/almacen/src/model/Venta.java
+++ src/main/java/com/almacen/src/model/Venta.java
@@ -1,4 +1,14 @@
 package com.almacen.src.model;
 
 public class Venta {
+
+    protected int codigo;
+
+    public Venta(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
 }
